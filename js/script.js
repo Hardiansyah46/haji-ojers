1cafc215e83abea59724dfe85c00ccea34662ca2
Aji Hardiansyah <ajihardinasah123@gmail.com>
2025-07-17 13:03:48 +0700

    membuat website berita capruk sederhana

--- js/script.js
+++ js/script.js
@@ -17,3 +17,19 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+{
+  /* <script> */
+}
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+const searchButton = document.querySelector("#search-button");
+
+searchButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  searchForm.classList.toggle("active");
+  if (searchForm.classList.contains("active")) {
+    searchBox.focus();
+  }
+});
+// </script>
